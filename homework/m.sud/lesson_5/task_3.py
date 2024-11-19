0e096b2242ac6d4ffaff78002913885f9d6eafed
Mikalai <mikalai.sud@gmail.com>
2024-11-19 03:03:53 +0300

    added few metod

--- homework/m.sud/lesson_5/task_3.py
+++ homework/m.sud/lesson_5/task_3.py
@@ -18,3 +18,15 @@ subject1, subject2, subject3 = subjects
 
 my_text = f'Students: {student1}, {student2}, {student3} study these subjects: {subject1}, {subject2}, {subject3}.'
 print(my_text)
+
+print("Students: " + student1 + ", " + student2 + ", " + student3 + " " + "study these subjects:",
+      subject1 + ", " + subject2 + ", " + subject3 + ".")
+
+my_text1 = "Students: %s, %s, %s study these subjects: %s, %s, %s."
+print(my_text1 % (student1, student2, student3, subject1, subject2, subject3))
+
+my_text2 = "Students: {}, {}, {} study these subjects: {}, {}, {}."
+print(my_text2.format(student1, student2, student3, subject1, subject2, subject3))
+
+my_text3 = "Students: {2}, {1}, {0} study these subjects: {4}, {3}, {5}."
+print(my_text3.format(student3, student2, student1, subject2, subject1, subject3))
