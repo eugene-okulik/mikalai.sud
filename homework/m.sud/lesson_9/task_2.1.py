ad5dedf04e9b0af268b96d860479140dba710862
Mikalai <mikalai.sud@gmail.com>
2024-12-11 15:19:46 +0300

    correct

--- homework/m.sud/lesson_9/task_2.1.py
+++ homework/m.sud/lesson_9/task_2.1.py
@@ -11,12 +11,17 @@
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32,
                 30, 28, 24, 23]
 
-new_list = []
-for x in temperatures:
-    if x > 28:
-        new_list.append(x)
 
-print(f"Отсортированный список {new_list}")
+def temp_28(x):
+    if x > 28:
+        return True
+    else:
+        return False
+
+
+new_list = list(filter(temp_28, temperatures))
+
+print(f"Отсортированный список {list(new_list)}")
 print(f"Минимальное - {min(new_list)}")
 print(f"Максимальное - {max(new_list)}")
-print(f"Среднее - {sum(new_list) / len(new_list)}")
+print(f"Среднее - {round(sum(new_list) / len(new_list), 1)}")
